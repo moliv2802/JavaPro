b7fe4cae210c650e538ff9e437fcae51caa0401b
moliv2802 <50334418+moliv2802@users.noreply.github.com>
2019-12-19 21:56:06 +0300

    added ArraySorting

--- scr/ru/geekbrains/arraysorting/ArraySorting.java
+++ scr/ru/geekbrains/arraysorting/ArraySorting.java
@@ -1,5 +1,98 @@
 package ru.geekbrains.arraysorting;
 
-public class ArraySorting {
+import java.util.Comparator;
+
+public class ArraySorting <T extends Comparable>{
+    private T[] list;
+    private int size;
+    private final int DEFAULT_CAPACITY = 10;
+
+    public ArraySorting(int capacity){
+        if (capacity <= 0){
+            throw new IllegalArgumentException("capacity: " + capacity);
+        }
+        list = (T[]) new Comparable[capacity];
+    }
+    public ArraySorting(){
+        list = (T[]) new Comparable[DEFAULT_CAPACITY];
+    }
+
+    public void add(T item){
+        list[size] = item;
+        size++;
+    }
+
+    public void add(int index, T item){
+        for (int i = size; i > index ; i--) {
+            list[i] = list[i - 1];
+        }
+        list[index] = item;
+        size++;
+    }
+    public void remove(int index){
+        for (int i = index; i < size ; i++) {
+            list[i] = list[i + 1];
+        }
+        size--;
+        list[size] = null;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[ ");
+        for (int i = 0; i < size - 1; i++) {
+            sb.append(list[i] + ", ");
+        }
+        sb.append(list[size - 1] + " ]");
+        return sb.toString();
+    }
+
+    private void swap(int index1, int index2) {
+        T temp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = temp;
+    }
+
+    public void selectionSort() {
+        for (int i = 0; i < size - 1; i++) {
+            int imin = i;
+            for (int j = i + 1; j < size; j++) {
+                if( list[j].compareTo(list[imin])<0){
+                    imin = j;
+                }
+            }
+            swap(i, imin);
+        }
+    }
+
+    public void insertionSort(){
+        T key;
+        for (int i = 1; i <size ; i++) {
+            int j = i;
+            key  = list[i];
+            while(j>0 && key.compareTo(list[j-1])<0){
+                list[j] = list[j-1];
+                j--;
+            }
+            list[j] = key;
+        }
+    }
+    public void bubbleSort(Comparator<T> comparator){
+        boolean isSwap;
+        for (int i = size-1; i > 0 ; i--) {
+            isSwap = false;
+            for (int j = 0; j < i ; j++) {
+                if( comparator.compare(list[j+1],list[j])<0 ){
+                    swap(j+1, j);
+                    isSwap = true;
+                }
+            }
+            if(!isSwap){
+                break;
+            }
+        }
+    }
+
 
 }
+
+
--- scr/ru/geekbrains/arraysorting/Main.java
+++ scr/ru/geekbrains/arraysorting/Main.java
@@ -4,6 +4,23 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        Random rand = new Random();
+        ArraySorting<Integer> arr = new ArraySorting<>(1000000);
+        for (int i = 0; i < 999999 ; i++) {
+            arr.add(rand.nextInt(10));
+        }
+        long sSel = System.currentTimeMillis();
+        System.out.println(sSel);
+        arr.selectionSort();
+        long fSel = System.currentTimeMillis();
+        System.out.println(fSel);
+        System.out.println("Время выполнения сортировки методом выбора: " + (fSel - sSel));
+        System.out.println(arr);
+
+
+
+
+
 
     }
 }
