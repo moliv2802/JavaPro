603d5673f3da5b779882774caf684048a22018ea
moliv2802 <50334418+moliv2802@users.noreply.github.com>
2019-12-19 20:40:51 +0300

    added ArraySorting

---
+++ scr/ru/geekbrains/arraysorting/ArraySorting.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.arraysorting;
+
+public class ArraySorting {
+
+}
---
+++ scr/ru/geekbrains/arraysorting/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.arraysorting;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
